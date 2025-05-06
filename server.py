461973a4db590157bc35da97c2129500ca0d5ced
pedrorfdez <pfrascon24@gmail.com>
2025-05-07 00:01:34 +0200

    Renamed to setup.py

---
+++ server.py
@@ -0,0 +1,24 @@
+from fastmcp import FastMCP
+from dotenv import load_dotenv
+import yfinance as yf
+
+load_dotenv()
+
+# Initialize FastMCP server
+mcp = FastMCP(
+    name="StockMarketServer",
+    instructions="You are a stock market server that can answer questions about the stock market.",
+    )
+
+@mcp.tool()
+async def get_stock_info(symbol: str) -> float:
+    """Get information from a stock. It may have a delay of up to 15 minutes."""
+    return yf.Ticker(symbol).info
+
+@mcp.tool()
+async def get_current_stock_price(symbol: str) -> float:
+    """Get the current price of a stock. It may have a delay of up to 15 minutes."""
+    return yf.Ticker(symbol).info["regularMarketPrice"],yf.Ticker(symbol).info['financialCurrency']
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
